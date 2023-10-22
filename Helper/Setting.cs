bac4bd7480d5797afcf2dcfc92fd251f7afc068f
Văn Thạnh <thanhhero765@gmail.com>
2023-10-22 21:31:42 +0700

    Add project files.

---
+++ Helper/Setting.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorVideo.Helper
+{
+    public class Setting
+    {
+        public Setting()
+        {
+        }
+
+        public int VideoIdx { get; set; } = 0;
+
+        public readonly float[] VideoSpeedRates = new[] { 0.25f, 0.5f, 0.75f, 1f, 1.25f, 1.5f, 1.75f, 2.0f };
+        public float VideoSpeedRate { get; set; } = 1.0f;
+
+        private bool _playstate = false;
+
+        public bool IsPlaying
+        {
+            get => _playstate;
+            set { _playstate = value; }
+        }
+
+        public bool IsPaused
+        {
+            get => _playstate ? false : true;
+            set { _playstate = !value; }
+        }
+
+        private float _SoundRate = 0.5f;
+
+        public float SoundRate
+        {
+            get => _SoundRate;
+            set
+            {
+                _SoundRate = value;
+                _SoundRate = Math.Max(_SoundRate, 0);
+                _SoundRate = Math.Min(_SoundRate, 1);
+            }
+        }
+
+        private bool _ShowingSetting = false;
+        public bool _SpeedSetting = false;
+        private bool _QualitySetting = false;
+
+        public bool ShowingSetting
+        {
+            get => _ShowingSetting;
+            set
+            {
+                _ShowingSetting = value;
+                if (_ShowingSetting == false)
+                {
+                    _SpeedSetting = _QualitySetting = false;
+                }
+            }
+        }
+
+        public bool SpeedSetting
+        {
+            get => _SpeedSetting;
+            set
+            {
+                _SpeedSetting = value;
+                if (_SpeedSetting == true)
+                {
+                    _QualitySetting = false;
+                }
+            }
+        }
+
+        public bool QualitySetting
+        {
+            get => _QualitySetting;
+            set
+            {
+                _QualitySetting = value;
+                if (_QualitySetting == true)
+                {
+                    _SpeedSetting = false;
+                }
+            }
+        }
+
+        public bool Mute { get; set; } = false;
+        public bool Subtitle { get; set; }
+
+        private bool _fullScreen = false;
+        private bool _pip = false;
+        private bool _cinema = false;
+
+        public bool FullScreen
+        {
+            get => _fullScreen;
+            set
+            {
+                _fullScreen = value;
+                if (_fullScreen) _pip = _cinema = false;
+            }
+        }
+
+        public bool PiP
+        {
+            get => _pip;
+            set
+            {
+                _pip = value;
+                if (_pip)
+                {
+                    _fullScreen = _cinema = false;
+                }
+            }
+        }
+
+        public bool Cinema
+        {
+            get => _cinema;
+            set
+            {
+                _cinema = value;
+                if (_cinema)
+                {
+                    _fullScreen = _pip = false;
+                }
+            }
+        }
+    }
+}
+}
---
+++ Helper/VideoHelper.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorVideo.Helper
+{
+    internal class VideoHelper
+    {
+    }
+
+    public class VideoDimension
+    {
+        public VideoDimension()
+        {
+        }
+
+        public VideoDimension(int height, int width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public int Height { get; set; }
+        public int Width { get; set; }
+    }
+
+    public class Media
+    {
+        public Media()
+        {
+        }
+
+        public Media(string[] sources, VideoDimension videoDimension, string poster)
+        {
+            Sources = sources;
+            Dimension = videoDimension;
+            Poster = poster;
+        }
+
+        private string[] _sources;
+        public double Duration { get; set; }
+
+        public TimeSpan DurationTimeSpan
+        {
+            get => TimeSpan.FromSeconds(Duration);
+        }
+
+        public double CurrentTime { get; set; }
+
+        public TimeSpan CurrentTimeTimeSpan
+        {
+            get => TimeSpan.FromSeconds(CurrentTime);
+        }
+
+        public double BufferedStart { get; set; }
+        public double BufferedEnd { get; set; }
+        public double BufferedLength { get; set; }
+        public bool Muted { get; set; }
+        public float Volumn { get; set; }
+        public bool Loop { get; set; }
+        public float DefaultPlaybackRate { get; set; }
+        public bool Paused { get; set; }
+        public float PlaybackRate { get; set; }
+
+        public string TimeSpanString
+        {
+            get { return $"{timeInSecToTimeString(CurrentTime)} / {timeInSecToTimeString(Duration)}"; }
+        }
+
+        public string[] Sources
+        {
+            get => _sources;
+
+            set
+            {
+                var srcs = from src in _sources where !string.IsNullOrEmpty(src) select src;
+                _sources = srcs.ToArray();
+            }
+        }
+
+        public VideoDimension Dimension { get; set; }
+        public string Poster { get; set; }
+
+        public string[] types
+        {
+            get
+            {
+                List<string> type = new List<string>();
+                foreach (var source in Sources)
+                {
+                    string mediatype = source.Split(".").Last().ToLower();
+                    switch (mediatype)
+                    {
+                        case "mp4":
+                        {
+                            type.Add("video/mp4");
+                            break;
+                        }
+                        case "ogg":
+                        {
+                            type.Add("video/ogg");
+                            break;
+                        }
+                        case "webm":
+                        {
+                            type.Add("video/webm");
+                            break;
+                        }
+                        case "mp3":
+                        {
+                            type.Add("audio/mpeg");
+                            break;
+                        }
+                        default:
+                        {
+                            type.Add("");
+                            break;
+                        }
+                    }
+                }
+
+                return type.ToArray();
+            }
+        }
+
+        private string timeInSecToTimeString(double time_)
+        {
+            var sec = time_ % (24 * 3600);
+            var hours = Math.Floor(sec / 3600);
+            sec %= 3600;
+            var minus = Math.Floor(sec / 60);
+            sec %= 60;
+            return $"{hours}:{minus}:{Math.Floor(sec)}";
+        }
+    }
+}
